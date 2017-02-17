fd0ffd5d50ed3c8ca357f325cf59a1079ff96b47
Mohk <Mohkmok@gmail.com>
2017-02-17 10:17:03 +0700

    Send agian

--- src/grass.java
+++ src/grass.java
@@ -1,8 +1,9 @@
 
-/**
- * @author Suphawit Kasemsirisopha [572115056]
- * on Feb 17, 2017
- */
-public class grass {
 
+
+public class grass {
+	public static void main (String []args){
+		
+	System.out.print("grow");
+	}
 }
